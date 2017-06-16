3756ccf2d4058f26e4a124ccf5263455c3cf2568
Nir Mekin <nir.mekin@gmail.com>
2017-06-16 21:06:03 +0300

    adding routing to main index and method to secondary index

--- app/index.js
+++ app/index.js
@@ -15,12 +15,120 @@ conn.on('error', (err) => {
 });
 
 class MusicPlayer {
+    // SONGS Methods
     getAllSongs() {
         return new Promise((resolve, reject) => {
             Songs.find({}, '-_id',
                 (err, result) => {
-                    if (err) reject(err);
-                    else resolve(result);
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"No Songs were found"});
+                    }
+                });
+        });
+    }
+
+    getSongsByTitle(_title) {
+        return new Promise((resolve, reject) => {
+            Songs.find({title:_title}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"the specific Song was not found"});
+                    }
+                });
+        });
+    }
+
+    getSongsByGenre(_genre) {
+        return new Promise((resolve, reject) => {
+            Songs.find({genre:_genre}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"Genre doesn't exist"});
+                    }
+                });
+        });
+    }
+
+    getSongsByArtist(_artist) {
+        return new Promise((resolve, reject) => {
+            Songs.find({artist:_artist}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"Artist doesn't exist"});
+                    }
+                });
+        });
+    }
+
+    // USERS Methods
+    getAllUsers() {
+        return new Promise((resolve, reject) => {
+            Users.find({}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"No Users were found"});
+                    }
+                });
+        });
+    }
+
+    getUserByID(_id) {
+        return new Promise((resolve, reject) => {
+            Users.find({id:_id}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"No Users were found"});
+                    }
+                });
+        });
+    }
+
+    // MIXES Methods
+    getAllMixes() {
+        return new Promise((resolve, reject) => {
+            Mixes.find({}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"No Users were found"});
+                    }
+                });
+        });
+    }
+    getMixesByUserID(_id){
+        return new Promise((resolve, reject) => {
+            Mixes.find({userid:_id}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"No Users were found"});
+                    }
+                });
+        });
+    }
+    getMixesByHashtags(_tag){
+        return new Promise((resolve, reject) => {
+            Mixes.find({hashtags:_tag}, '-_id',
+                (err, result) => {
+                    if (err) throw reject(err);
+                    else{
+                        if(result.length!=0) resolve(result)
+                        else result({"Error":"No Users were found"});
+                    }
                 });
         });
     }
--- index.js
+++ index.js
@@ -5,9 +5,6 @@
 const   express     = require('express'),
         bodyParser  = require('body-parser'),
         app         = express(),
-        songs       = require('./songs_mdl'),
-        users       = require('./users_mdl'),
-        mixes       = require('./mixes_mdl'),
         musicPlayer = require('./app/index'),
         port        = process.env.PORT || 3000;
 
@@ -15,25 +12,65 @@ const   express     = require('express'),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//get instance of class musicPlayer
 var mPlayer = musicPlayer();
+
+//Abstract Function for relevant method matching the rout
+function abstractFunction(res,fun,param1,param2){
+    try{
+       fun(param1,param2).then((result) => {
+            res.status(200).json(result);
+            console.log(`==========\n${result}\n Received successfully from the database!\n==========`);
+        })
+    }catch(err) {
+        console.log(`==========\nFailed to retrieve data: ${err}\n==========`);
+        res.status(200).json({"Error":"DB issue"});
+    }
+}
 
 app.all('*', (req, res, next) => {
     console.log('==== Request received ====');
     req.next();
 });
 
+// SONGS Routes
 app.get('/getAllSongs', (req, res) => {
-    mPlayer.getAllSongs().then((result) => {
-        if (result.length !== 0) {
-            console.log(`==========\n${result}\n Received successfully from the database!\n==========`);
-            res.status(200).json(result);
-        }
-        else {
-            console.log(`==========\nFailed to retrieve data\n==========`);
-            res.status(200).json(`Failed to retrieve data`);
-        }
-    });
+    abstractFunction(res,mPlayer.getAllSongs);
 });
+
+app.get('/getSongsByTitle/:title', (req, res) => {
+    abstractFunction(res,mPlayer.getSongsByTitle,req.params.title);
+});
+
+app.post('/getSongsByGenre/',(req,res)=> {
+    abstractFunction(res,mPlayer.getSongsByGenre,req.body.genre);
+});
+
+app.get(`/getSongsByArtist/:artist`,(req,res)=>{
+    abstractFunction(res,mPlayer.getSongsByArtist,req.params.artist);
+})
+
+// USERS Routes
+app.get(`/getAllUsers`,(req,res)=>{
+    abstractFunction(res,mPlayer.getAllUsers);
+})
+
+app.get(`/getUserByID/:id`,(req,res)=>{
+    abstractFunction(res,mPlayer.getUserByID,req.params.id);
+})
+
+// MIXES Routes
+app.get(`/getAllMixes`,(req,res)=>{
+    abstractFunction(res,mPlayer.getAllMixes);
+})
+
+app.get(`/getMixesByUserID/:id`,(req,res)=>{
+    abstractFunction(res,mPlayer.getMixesByUserID,req.params.id);
+})
+
+app.post('/getMixesByHashtags',(req,res)=>{
+    abstractFunction(res,mPlayer.getMixesByHashtags,req.body.hashtag);
+})
 
 app.listen(port);
 console.log(`listening on port ${port}`);
