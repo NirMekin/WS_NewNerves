d29e23b69bd007f3bbe33a6d5e908bc46fad995c
Nir Mekin <nir.mekin@gmail.com>
2017-06-16 22:23:21 +0300

    changes

--- app/index.js
+++ app/index.js
@@ -14,123 +14,56 @@ conn.on('error', (err) => {
     console.log(`connection error: ${err}`);
 });
 
+//Abstract Find query
+function abstractFindModel(model,_query,errMsg){
+    return new Promise((resolve, reject) => {
+        model.find(_query ,'-_id',
+            (err, result) => {
+                if (err)  reject(err);
+                else{
+                    if(result.length!=0) resolve(result);
+                    else resolve(errMsg);
+                }
+            });
+    });
+}
 class MusicPlayer {
-    // SONGS Methods
-    getAllSongs() {
-        return new Promise((resolve, reject) => {
-            Songs.find({}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"No Songs were found"});
-                    }
-                });
-        });
+
+    getAllSongs(){
+        return abstractFindModel(Songs,{},{"Error":"No Songs were found"});
     }
 
-    getSongsByTitle(_title) {
-        return new Promise((resolve, reject) => {
-            Songs.find({title:_title}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"the specific Song was not found"});
-                    }
-                });
-        });
+    getSongsByTitle(_title){
+        return abstractFindModel(Songs,{title:_title},{"Error":"the specific Song was not found"});
     }
 
     getSongsByGenre(_genre) {
-        return new Promise((resolve, reject) => {
-            Songs.find({genre:_genre}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"Genre doesn't exist"});
-                    }
-                });
-        });
+        return abstractFindModel(Songs,{genre:_genre},{"Error":"Genre doesn't exist"});
     }
 
     getSongsByArtist(_artist) {
-        return new Promise((resolve, reject) => {
-            Songs.find({artist:_artist}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"Artist doesn't exist"});
-                    }
-                });
-        });
+        return abstractFindModel(Songs,{artist:_artist},{"Error":"Artist doesn't exist"});
     }
 
     // USERS Methods
     getAllUsers() {
-        return new Promise((resolve, reject) => {
-            Users.find({}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"No Users were found"});
-                    }
-                });
-        });
+        return abstractFindModel(Users,{},{"Error":"No Users were found"});
     }
 
     getUserByID(_id) {
-        return new Promise((resolve, reject) => {
-            Users.find({id:_id}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"No Users were found"});
-                    }
-                });
-        });
+        return abstractFindModel(Users,{id:_id},{"Error":"Users was not found"});
     }
 
     // MIXES Methods
     getAllMixes() {
-        return new Promise((resolve, reject) => {
-            Mixes.find({}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"No Users were found"});
-                    }
-                });
-        });
+        return abstractFindModel(Mixes,{},{"Error":"No Mixes were found"});
     }
     getMixesByUserID(_id){
-        return new Promise((resolve, reject) => {
-            Mixes.find({userid:_id}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"No Users were found"});
-                    }
-                });
-        });
+        return abstractFindModel(Mixes,{userid:_id},{"Error":"No Mixes for user were found"});
     }
+
     getMixesByHashtags(_tag){
-        return new Promise((resolve, reject) => {
-            Mixes.find({hashtags:_tag}, '-_id',
-                (err, result) => {
-                    if (err) throw reject(err);
-                    else{
-                        if(result.length!=0) resolve(result)
-                        else result({"Error":"No Users were found"});
-                    }
-                });
-        });
+        return abstractFindModel(Mixes,{hashtags:_tag},{"Error":"No Mixes with current hashtag were found"});
     }
 
 }
